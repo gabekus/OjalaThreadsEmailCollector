1dfe286f08d3459db247802148a68982e14fc5bd
Gabe <kuslanskyg@gmail.com>
2018-08-09 15:27:22 -0400

    Added download route

--- src/modules/routing.js
+++ src/modules/routing.js
@@ -1,11 +1,16 @@
 import { Router } from 'express';
 import { logUser } from '.';
+import { emailFile } from '../app';
 
 const router = Router();
 
 router.post('/email', (req) => {
   const { name, email } = req.body;
   logUser(name, email);
+});
+
+router.get('/download', (req, res) => {
+  res.download(emailFile);
 });
 
 export const routing = router;
