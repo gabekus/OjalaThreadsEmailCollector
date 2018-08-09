51f4ac2e00446878a799400f5f3053d1f109dd53
Gabe <kuslanskyg@gmail.com>
2018-08-08 22:59:57 -0400

    Added routing

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
-import * as express from 'express';
+import express from 'express';
 import { Server } from 'http';
+import { routing } from './modules';
 
 const app = express();
 const server = new Server(app);
+
+app.use(routing);
 
 server.listen(3000);
---
+++ src/modules/routing.js
@@ -0,0 +1,9 @@
+import { Router } from 'express';
+
+const router = Router();
+
+router.post('/email', () => {
+
+});
+
+export const routing = router;
