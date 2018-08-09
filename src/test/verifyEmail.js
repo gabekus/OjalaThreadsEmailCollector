e700594e2d33eeb509a1df07e200252c7e5589a3
Gabe <kuslanskyg@gmail.com>
2018-08-09 00:01:34 -0400

    Added unit tests for email verification

---
+++ src/test/verifyEmail.js
@@ -0,0 +1,18 @@
+import { describe, it } from 'mocha';
+import * as chai from 'chai';
+import { verifyEmail } from '../modules/verifyEmail';
+
+const { expect } = chai;
+
+const verifiedEmails = ['bob123@gmail.com', 'gibson@yahoo.com', '23stacys14Mom@nyc.rr.com'];
+const fakeEmails = ['fake input', 'test', 'j203f02', '_DJ*@(#', 'gmail @com.gmail'];
+
+describe('verifyEmail', () => {
+  it('should verify all the following emails', () => {
+    verifiedEmails.forEach(email => expect(verifyEmail(email)).to.equal(true));
+  });
+
+  it('should NOT verify all the following emails', () => {
+    fakeEmails.forEach(email => expect(verifyEmail(email)).to.equal(false));
+  });
+});
