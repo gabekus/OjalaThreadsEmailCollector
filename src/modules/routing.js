77ad6c044fccd6431fdb9a1486b8c2b15b7e4dae
Gabe <kuslanskyg@gmail.com>
2018-08-13 16:11:20 -0400

    Moved HTTP/Headers out of node and into nginx config

--- src/modules/routing.js
+++ src/modules/routing.js
@@ -20,15 +20,6 @@ router.post('/email', async (req, res) => {
   if (responseMsg === 'Successfully registered') {
     logger.info(`${name} signed up with ${email}`);
   }
-
-  // CORS request fails if these headers don't exist
-  const headers = {};
-  headers['Access-Control-Allow-Origin'] = '*';
-  headers['Access-Control-Allow-Methods'] = 'POST';
-  headers['Access-Control-Allow-Credentials'] = false;
-  headers['Access-Control-Max-Age'] = '86400'; // 24 hours
-  headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept';
-  res.writeHead(200, headers);
   res.end(responseMsg);
 });
 
